e2e4102256794ef2b35acdd2836817a37fa43d69
Neeraj Soni <neersoni@codeaurora.org>
2018-12-14 15:18:15 +0530

    system: vold: Use wrapped key for metadata encryption
    Wrapped key feature is needed for better security of encryption keys and to
    ensure data integrity when crypto key cache is cleared during reset operation
    of storage/crypto hardware.
    
    Original patch: https://source.codeaurora.org/quic/la/platform/system/vold/commit/?h=LA.QSSI.11.0.r1-05600-qssi.0&id=c480f913e6abc2757c0d79afba5a3df1c4adc731
    [Pig]: Clean up all deprecated codes that were removed during latter
    merge.
    
    CRs-Fixed: 2367150
    Change-Id: I83d14861bf81e102151fa3417d84008c214a9ac0

--- FsCrypt.h
+++ FsCrypt.h
@@ -43,3 +43,4 @@ bool fscrypt_destroy_user_storage(const std::string& volume_uuid, userid_t user_
 bool fscrypt_destroy_volume_keys(const std::string& volume_uuid);
 bool lookup_key_ref(const std::map<userid_t, android::fscrypt::EncryptionPolicy>& key_map, userid_t user_id,
                            std::string* raw_ref);
+bool is_metadata_wrapped_key_supported();
