8ea5d1de1da4d65cdf35b8038c28c6409e20f492
Mohd Faraz <androiabledroid@gmail.com>
2023-01-07 21:59:46 +0100

    vold: dynamically choose fscrypt policy [2/2]
    Change-Id: I076f5aecaa9102e2d14c80bd9a4daacaaa3b0e7b
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- fscrypt_policy.h
+++ fscrypt_policy.h
@@ -19,26 +19,19 @@
 
 #include <sys/cdefs.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <cutils/multiuser.h>
 #include <linux/fs.h>
 
 __BEGIN_DECLS
 
 #define FS_KEY_DESCRIPTOR_SIZE_HEX (2 * FS_KEY_DESCRIPTOR_SIZE + 1)
-#define FSCRYPT_KEY_IDENTIFIER_HEX_SIZE ((2 * FSCRYPT_KEY_IDENTIFIER_SIZE) + 1)
+#define FSCRYPT_KEY_IDENTIFIER_SIZE_HEX ((2 * FSCRYPT_KEY_IDENTIFIER_SIZE) + 1)
 
-#ifdef USE_FSCRYPT_POLICY_V1
-#define USER_CE_FSCRYPT_POLICY           "0CE"
-#define USER_DE_FSCRYPT_POLICY           "0DE"
-#define SYSTEM_DE_FSCRYPT_POLICY         "0DK"
-#else
-#define USER_CE_FSCRYPT_POLICY           "2CE"
-#define USER_DE_FSCRYPT_POLICY           "2DE"
-#define SYSTEM_DE_FSCRYPT_POLICY         "2DK"
-#endif
+#define USER_CE_FSCRYPT_POLICY           "CE"
+#define USER_DE_FSCRYPT_POLICY           "DE"
+#define SYSTEM_DE_FSCRYPT_POLICY         "DK"
 
-#define FSCRYPT_V1                        "0"
-#define FSCRYPT_V2                        "2"
 #define SYSTEM_DE_KEY                     "DK"
 #define USER_CE_KEY                       "C"
 #define USER_DE_KEY                       "D"
@@ -61,27 +54,32 @@ __BEGIN_DECLS
 
 bool fscrypt_set_mode();
 
-#ifdef USE_FSCRYPT_POLICY_V1
-bool lookup_ref_key(struct fscrypt_policy_v1 *fep, uint8_t* policy_type);
-#else
-bool lookup_ref_key(struct fscrypt_policy_v2 *fep, uint8_t* policy_type);
-#endif
+#undef fscrypt_policy
+typedef union fscrypt_policy {
+	uint8_t version;
+	struct fscrypt_policy_v1 v1;
+	struct fscrypt_policy_v2 v2;
+} fscrypt_policy;
 
-bool lookup_ref_tar(const uint8_t *policy_type, uint8_t *policy);
+bool lookup_ref_key(fscrypt_policy *fep, uint8_t* policy_type);
 
-#ifdef USE_FSCRYPT_POLICY_V1
-bool fscrypt_policy_get_struct(const char *directory, struct fscrypt_policy_v1  *fep);
-#else
-bool fscrypt_policy_get_struct(const char *directory, struct fscrypt_policy_v2  *fep);
-#endif
+bool lookup_ref_tar(fscrypt_policy *fep, uint8_t *policy);
 
-#ifdef USE_FSCRYPT_POLICY_V1
-bool fscrypt_policy_set_struct(const char *directory, const struct fscrypt_policy_v1  *fep);
-#else
-bool fscrypt_policy_set_struct(const char *directory, const struct fscrypt_policy_v2  *fep);
-#endif
+bool fscrypt_policy_get_struct(const char *directory, fscrypt_policy *fep);
+
+bool fscrypt_policy_set_struct(const char *directory, fscrypt_policy  *fep);
 
 void bytes_to_hex(const uint8_t *bytes, size_t num_bytes, char *hex);
+
+uint8_t* get_policy_descriptor(fscrypt_policy* fep);
+
+void* get_policy(const fscrypt_policy* fep);
+
+uint8_t get_policy_size(fscrypt_policy* fep, bool hex);
+
+int fscrypt_policy_size(const fscrypt_policy *policy);
+
+void get_policy_content(fscrypt_policy* fep, char* content);
 __END_DECLS
 
 #endif // _FS_CRYPT_H_
